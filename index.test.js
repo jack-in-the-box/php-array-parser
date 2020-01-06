53d96090f8572592b2a7ac2e7b085355ae8fa1d4
Nicolas PETRINI <npetrini@jack.fr>
2020-01-06 09:53:45 +0100

    Add test for float values

--- index.test.js
+++ index.test.js
@@ -1,32 +1,42 @@
-var parser = require('./index')
+var parser = require("./index");
 
-test('simple array', () => {
+test("simple array", () => {
   var parsed = parser.parse(`
     array(
       "foo" => "bar",
       "bar" => "foo",
     );
-  `)
+  `);
   expect(parsed).toEqual({
-    foo: 'bar',
-    bar: 'foo'
-  })
-})
+    foo: "bar",
+    bar: "foo"
+  });
+});
 
-test('simple array (as of PHP 5.4)', () => {
+test("simple array (as of PHP 5.4)", () => {
   var parsed = parser.parse(`
     [
       "foo" => "bar",
       "bar" => "foo",
     ];
-  `)
+  `);
   expect(parsed).toEqual({
-    foo: 'bar',
-    bar: 'foo'
-  })
-})
+    foo: "bar",
+    bar: "foo"
+  });
+});
 
-test('type casting', () => {
+test("preserve float values", () => {
+  var parsed = parser.parse(`
+    [
+      0.1,
+      0.2,
+    ];
+  `);
+  expect(parsed).toEqual([0.1, 0.2]);
+});
+
+test("type casting", () => {
   var parsed = parser.parse(`
     [
       true => "a",
@@ -34,28 +44,28 @@ test('type casting', () => {
       "3"  => "c",
       4.5  => "d",
     ];
-  `)
+  `);
   expect(parsed).toEqual({
-    '1': 'a',
-    '2': 'b',
-    '3': 'c',
-    '4': 'd',
-  })
-})
+    "1": "a",
+    "2": "b",
+    "3": "c",
+    "4": "d"
+  });
+});
 
-test('overwriting', () => {
+test("overwriting", () => {
   var parsed = parser.parse(`
     [
       1 => "a",
       true => "b",
     ];
-  `)
+  `);
   expect(parsed).toEqual({
-    '1': 'b',
-  })
-})
+    "1": "b"
+  });
+});
 
-test('mixed integer and string keys', () => {
+test("mixed integer and string keys", () => {
   var parsed = parser.parse(`
     [
       "foo" => "bar",
@@ -63,16 +73,16 @@ test('mixed integer and string keys', () => {
       100   => -100,
       -100  => 100,
     ];
-  `)
+  `);
   expect(parsed).toEqual({
-    'foo': 'bar',
-    'bar': 'foo',
+    foo: "bar",
+    bar: "foo",
     [100]: -100,
-    [-100]: 100,
-  })
-})
+    [-100]: 100
+  });
+});
 
-test('indexed array without key', () => {
+test("indexed array without key", () => {
   var parsed = parser.parse(`
     array(
       "foo",
@@ -89,23 +99,23 @@ test('indexed array without key', () => {
         "foo" => "bar"
       )
     );
-  `)
+  `);
   expect(parsed).toEqual([
-    'foo',
-    'bar',
-    'hello',
-    'world',
+    "foo",
+    "bar",
+    "hello",
+    "world",
     100,
     -100,
     true,
     false,
     [],
     [1],
-    { foo: 'bar' },
-  ])
-})
+    { foo: "bar" }
+  ]);
+});
 
-test('empty array', () => {
-  var parsed = parser.parse('array()')
-  expect(parsed).toEqual([])
-})
+test("empty array", () => {
+  var parsed = parser.parse("array()");
+  expect(parsed).toEqual([]);
+});
